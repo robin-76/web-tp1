eb09c71b7d63a783c6f65be5453cbac3cb3ea6cc
Robin Guyomar <robin.guyomar@etu.univ-lehavre.r>
2021-10-29 12:33:48 +0200

    mdp avec un minimum de caractere autorisé + adresse mail unique + hash mdp

--- Web_TP1/routes/signup.js
+++ Web_TP1/routes/signup.js
@@ -1,15 +1,26 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 const { validationResult } = require('express-validator');
 const router = express.Router();
 const User = mongoose.model('User');
+
 
 router.get('/', (req, res) => {
     res.render('signup', { title: 'Sign up' });
 });
 
-router.post('/', (req, res) => {
+router.post('/', async(req, res) => {
     const errors = validationResult(req);
+
+    // Checking if the user is already in the database
+    const emailExist = await User.findOne({email: req.body.email});
+    if(emailExist) return res.status(400).send('Email already exists')
+
+    // Hash passwords
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(req.body.password, salt);
+    req.body.password = hashedPassword;
 
     if (errors.isEmpty()) {
         const user = new User(req.body);
